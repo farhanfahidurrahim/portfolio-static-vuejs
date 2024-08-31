a68bc53c92b955e06db67ee0aa778af316738712
farhanfahidurrahim <farhan.fahidurrahim@gmail.com>
2024-08-31 14:12:01 +0600

    resume updateX2

--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -62,7 +62,7 @@
           </p>
           <div class="hero-buttons">
             <a
-              href="https://drive.google.com/file/d/1rCWErDJS6mYsJ5UXBghnnnqhW23JxGrF/view?usp=sharing"
+              href="https://drive.google.com/file/d/1SpehzFJGbyREG_qq8ubbznYLGoqnYopV/view?usp=sharing"
               target="_blank"
               class="btn btn-primary"
               >Download Resume</a
